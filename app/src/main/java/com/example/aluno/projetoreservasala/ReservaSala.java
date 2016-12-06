9177440adcf26817179966a38a7770535ffd69a6
alexandresebrao <alexandre.sebrao@gmail.com>
2016-12-06 16:48:27 -0200

    Fix Parse

--- app/src/main/java/com/example/aluno/projetoreservasala/ReservaSala.java
+++ app/src/main/java/com/example/aluno/projetoreservasala/ReservaSala.java
@@ -9,9 +9,17 @@ import com.parse.Parse;
  */
 public class ReservaSala extends Application{
     public void onCreate() {
+//        Para Servidor Local (Desenvolvimento)
+
+//        super.onCreate();
+//        Parse.initialize(new Parse.Configuration.Builder(this).applicationId("APPLICATION_ID")
+//        .clientKey(null).server("http://192.168.25.6:1337/parse/").build()
+//        );
+
+//      Para Versão Release ( Produção )
         super.onCreate();
-        Parse.initialize(new Parse.Configuration.Builder(this).applicationId("APPLICATION_ID")
-        .clientKey(null).server("http://192.168.25.6:1337/parse/").build()
+        Parse.initialize(new Parse.Configuration.Builder(this).applicationId("Pz4UEHg2wC0JJEXCJmpsj2zb17b6ONsw0K8KXGtX")
+                .clientKey("uPZk6eOa27yd1M1ErfScK9vku72f21z7x8gjJ4sO").server("https://parseapi.back4app.com/").build()
         );
     }
 }
